6f9cdba924767f8780366c65fbfe6cdc79b62743
Jaykumar Patel <jaykumarpatel@acies.holdings>
2024-11-28 23:54:07 +0530

    asdf

--- routers/home.py
+++ routers/home.py
@@ -1,3 +1,17 @@
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends, HTTPException, Path, Request, status,Query
+import json
+import database
+import pandas as pd
+import numpy as np
+templates = Jinja2Templates(directory="templates")
+
+
+
+
+
+
 from starlette import status
 from fastapi.templating import Jinja2Templates
 from fastapi import APIRouter, Depends, HTTPException, Path, Request, status
@@ -60,3 +74,42 @@ async def return_home(request: Request):
 
 
     return templates.TemplateResponse("home.html", {"request": request,"Data":art_list})
+
+
+
+
+
+
+
+
+
+
+# router = APIRouter(
+#     # prefix='/',
+#     tags=['home']
+# )
+
+
+# @router.get('/', status_code=status.HTTP_200_OK)
+# async def return_home(request: Request):
+
+    
+
+
+#     return templates.TemplateResponse("home.html", {"request": request})
+
+# @router.get('/api/{custom_string}', status_code=status.HTTP_200_OK)
+# async def return_home(custom_string,gid: str = Query(None), id: str = Query(None)):
+#     id = custom_string
+    
+
+#     r = (f'https://docs.google.com/spreadsheets/d/{id}/export?format=csv')
+#     if gid:
+#         r = (f'https://docs.google.com/spreadsheets/d/{id}/export?format=csv&gid={gid}')
+    
+#     data = pd.read_csv(r)
+#     data.replace([np.inf, -np.inf], np.nan, inplace=True)
+#     data = data.to_dict(orient='records')
+#     print(data)
+#     return JSONResponse(content=data)
+
